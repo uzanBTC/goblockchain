0e7c7d5a6dd78d7343547e7caadf9b93a884b98b
wzh6886668 <8774027+wzh6886668@user.noreply.gitee.com>
2022-07-31 12:11:16 +0800

    add hash calculation

--- blockchain.go
+++ blockchain.go
@@ -5,16 +5,40 @@ import (
 	"log"
 	"time"
 	"strings"
+	"crypto/sha256"
+	"encoding/json"
 )
 
 type Block struct {
 	nonce int
-	previousHash string
+	previousHash [32]byte
 	timestamp int64
 	transactions []string
 }
 
+func (b *Block) Hash() [32]byte {
+	m,_:=b.MarshalJSON()
+	fmt.Println(string(m))
+	return sha256.Sum256([]byte(m))
+}
 
+func (b *Block) MarshalJSON() ([]byte, error) {
+	return json.Marshal(struct{
+		Timestamp int64 `json:"timestamp"`
+		Nonce int `json:"nonce"`
+		PreviousHash [32]byte `json:"previous_hash"`
+		Transactions []string `json:"transactions"`
+	}{
+		Timestamp: b.timestamp,
+		Nonce: b.nonce,
+		PreviousHash: b.previousHash,
+		Transactions: b.transactions,
+	})
+}
+
+func (bc *Blockchain) LastBlock() *Block {
+	return bc.chain[len(bc.chain)-1]
+}
 
 type Blockchain struct {
 	transactionPool []string
@@ -22,18 +46,19 @@ type Blockchain struct {
 }
 
 func NewBlockchain() *Blockchain {
+	b := &Block{}
 	bc := new(Blockchain)
-	bc.CreateBlock(0,"Init hash")
+	bc.CreateBlock(0,b.Hash())
 	return bc
 }
 
-func (bc *Blockchain) CreateBlock(nonce int, previousHash string) *Block {
+func (bc *Blockchain) CreateBlock(nonce int, previousHash [32]byte) *Block {
 	b := NewBlock(nonce, previousHash)
 	bc.chain = append(bc.chain,b)
 	return b
 }
 
-func NewBlock(nonce int, previousHash string) *Block {
+func NewBlock(nonce int, previousHash [32]byte) *Block {
 	b := new(Block)
 	b.timestamp = time.Now().UnixNano()
 	b.nonce=nonce
@@ -44,7 +69,7 @@ func NewBlock(nonce int, previousHash string) *Block {
 func (b *Block) Print() {
 	fmt.Printf("timestamp      %d\n", b.timestamp)
 	fmt.Printf("nonce          %d\n", b.nonce)
-	fmt.Printf("previous_hash  %s\n", b.previousHash)
+	fmt.Printf("previous_hash  %x\n", b.previousHash)
 	fmt.Printf("transactions   %s\n", b.transactions)
 }
 
@@ -53,7 +78,7 @@ func (bc *Blockchain) Print() {
 		fmt.Printf("%s Chain %d %s\n",strings.Repeat("=",25),i,strings.Repeat("=",25))
 		block.Print()
 	}
-	fmt.Printf("%s\n",strings.Repeat("*",25))
+	fmt.Printf("%s\n",strings.Repeat("*",75))
 }
 
 
@@ -62,6 +87,16 @@ func init(){
 }
 
 func main() {
+	
 	blockChain := NewBlockchain()
 	blockChain.Print()
+
+	previousHash := blockChain.LastBlock().Hash()
+	blockChain.CreateBlock(5,previousHash)
+	blockChain.Print()
+
+	previousHash = blockChain.LastBlock().Hash()
+	blockChain.CreateBlock(2,previousHash)
+	blockChain.Print()
+
 }
