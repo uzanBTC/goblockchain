7b8d0656f1dca5e83b38c8ea6c252474427447c4
wzh6886668 <8774027+wzh6886668@user.noreply.gitee.com>
2022-07-31 10:42:19 +0800

    first commit

---
+++ blockchain.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"time"
+)
+
+type Block struct {
+	nonce int
+	previousHash string
+	timestamp int64
+	transactions []string
+}
+
+func NewBlock(nonce int, previousHash string) *Block {
+	b := new(Block)
+	b.timestamp = time.Now().UnixNano()
+	b.nonce=nonce
+	b.previousHash=previousHash
+	return b
+}
+
+func (b *Block) Print() {
+	fmt.Printf("timestamp      %d\n", b.timestamp)
+	fmt.Printf("nonce          %d\n", b.nonce)
+	fmt.Printf("previous_hash  %s\n", b.previousHash)
+	fmt.Printf("transactions   %s\n", b.transactions)
+}
+
+
+func init(){
+	log.SetPrefix("Blockchain: ")
+}
+
+func main() {
+	b := NewBlock(0,"init hash")
+	b.Print()
+}
