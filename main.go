ee140cb302d9244d09a4c9921d04153a2e84fea6
wzh6886668 <8774027+wzh6886668@user.noreply.gitee.com>
2022-08-07 14:17:31 +0800

    add transaction signature

--- main.go
+++ main.go
@@ -14,4 +14,8 @@ func main() {
 	w := wallet.NewWallet()
 	fmt.Println(w.PrivateKeyStr())
 	fmt.Println(w.PublicKeyStr())
+	fmt.Println(w.BlockchainAddress())
+
+	t := wallet.NewTransaction(w.PrivateKey(), w.PublicKey(), w.BlockchainAddress(), "B", 1.0)
+	fmt.Printf("signature %s\n", t.GenerateSignature())
 }
--- wallet/wallet.go
+++ wallet/wallet.go
@@ -4,19 +4,63 @@ import (
 	"crypto/ecdsa"
 	"crypto/elliptic"
 	"crypto/rand"
+	"crypto/sha256"
+	"encoding/json"
 	"fmt"
+	"github.com/btcsuite/btcutil/base58"
+	"golang.org/x/crypto/ripemd160"
+	"math/big"
 )
 
 type Wallet struct {
-	privateKey *ecdsa.PrivateKey
-	publicKey  *ecdsa.PublicKey
+	privateKey        *ecdsa.PrivateKey
+	publicKey         *ecdsa.PublicKey
+	blockchainAddress string
 }
 
 func NewWallet() *Wallet {
+	// 1. Creating ECDSA private key (32b) public key (64b)
 	w := new(Wallet)
 	privateKey, _ := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
 	w.privateKey = privateKey
 	w.publicKey = &w.privateKey.PublicKey
+	// 2. Perform SHA-256 hashing on the public key ( 32 bytes ).
+	h2 := sha256.New()
+	h2.Write(w.publicKey.X.Bytes())
+	h2.Write(w.publicKey.Y.Bytes())
+	digest2 := h2.Sum(nil)
+	// 3. Perform RIPEMD-160 hashing on the result of SHA-256 ( 20 bytes).
+	h3 := ripemd160.New()
+	h3.Write(digest2)
+	digest3 := h3.Sum(nil)
+
+	// 4. Add version byte in front of RIPEMD-160 hash
+	vd4 := make([]byte, 21)
+	vd4[0] = 0x00
+	copy(vd4[1:], digest3[:])
+
+	// 5. Perform SHA-256 hash on the extended RIPEMD-160 result.
+	h5 := sha256.New()
+	h5.Write(vd4)
+	digest5 := h5.Sum(nil)
+
+	// 6. Perform SHA-256 hash on the result of the previous SHA-256 hash.
+	h6 := sha256.New()
+	h6.Write(digest5)
+	digest6 := h6.Sum(nil)
+
+	// 7. Take the first 4 bytes of the second SHA-256 hash for checksum
+	chsum := digest6[:4]
+
+	// 8. Add the 4 checksum bytes from 7 at the end of extended RIPEMD-160 hash from 4 (25 bytes).
+	dc8 := make([]byte, 25)
+	copy(dc8[:21], vd4[:])
+	copy(dc8[21:], chsum[:])
+
+	// 9. Convert the result from a byte string into base58.
+	address := base58.Encode(dc8)
+	w.blockchainAddress = address
+
 	return w
 }
 
@@ -34,4 +78,49 @@ func (w *Wallet) PublicKey() *ecdsa.PublicKey {
 
 func (w *Wallet) PublicKeyStr() string {
 	return fmt.Sprintf("%x%x", w.publicKey.X.Bytes(), w.publicKey.Y.Bytes())
+}
+
+func (w *Wallet) BlockchainAddress() string {
+	return w.blockchainAddress
+}
+
+type Transaction struct {
+	senderPrivateKey           *ecdsa.PrivateKey
+	senderPublicKey            *ecdsa.PublicKey
+	senderBlockchainAddress    string
+	recipientBlockchainAddress string
+	value                      float32
+}
+
+func NewTransaction(privateKey *ecdsa.PrivateKey, publicKey *ecdsa.PublicKey,
+	sender string, recipient string, value float32) *Transaction {
+	return &Transaction{privateKey, publicKey, sender, recipient, value}
+}
+
+func (t *Transaction) GenerateSignature() *Signature {
+	m, _ := json.Marshal(t)
+	h := sha256.Sum256([]byte(m))
+	r, s, _ := ecdsa.Sign(rand.Reader, t.senderPrivateKey, h[:])
+	return &Signature{r, s}
+}
+
+func (t *Transaction) MarshalJSON() ([]byte, error) {
+	return json.Marshal(struct {
+		Sender    string  `json:"sender_blockchain_address"`
+		Recipient string  `json:"recipient_blockchain_address"`
+		Value     float32 `json:"value"`
+	}{
+		Sender:    t.senderBlockchainAddress,
+		Recipient: t.recipientBlockchainAddress,
+		Value:     t.value,
+	})
+}
+
+type Signature struct {
+	R *big.Int
+	S *big.Int
+}
+
+func (s *Signature) String() string {
+	return fmt.Sprintf("%x%x", s.R, s.S)
 }
