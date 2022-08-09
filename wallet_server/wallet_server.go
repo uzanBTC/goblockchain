83ed6b60e1c9cdc806da801eb7fefec1af1533c9
wzh6886668 <8774027+wzh6886668@user.noreply.gitee.com>
2022-08-09 23:59:36 +0800

    link wallet address to index.html

--- wallet_server/wallet_server.go
+++ wallet_server/wallet_server.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"goblockchain/wallet"
 	"html/template"
+	"io"
 	"log"
 	"net/http"
 	"path"
@@ -37,7 +39,21 @@ func (ws *WalletServer) Index(w http.ResponseWriter, req *http.Request) {
 	}
 }
 
+func (ws *WalletServer) Wallet(w http.ResponseWriter, req *http.Request) {
+	switch req.Method {
+	case http.MethodPost:
+		w.Header().Add("Content-Type", "application/json")
+		myWallet := wallet.NewWallet()
+		m, _ := myWallet.MarshalJSON()
+		io.WriteString(w, string(m[:]))
+	default:
+		w.WriteHeader(http.StatusBadRequest)
+		log.Println("ERROR: Invalid HTTP Method")
+	}
+}
+
 func (ws *WalletServer) Run() {
 	http.HandleFunc("/", ws.Index)
+	http.HandleFunc("/wallet", ws.Wallet)
 	log.Fatal(http.ListenAndServe("127.0.0.1:"+strconv.Itoa(int(ws.Port())), nil))
 }
